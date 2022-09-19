bd8727756a370faf94a8da8fedba1a748de8a308
Daniel Ruiz <ruizdaniel.e@yahoo.com>
2022-09-19 13:58:58 -0500

    reliable for in loop

--- For-Of-Loops&Objects/ForOfLoops&Objects.js
+++ For-Of-Loops&Objects/ForOfLoops&Objects.js
@@ -8,7 +8,15 @@ const sportsCar = Object.create(car);
 sportsCar.speed = "fast";
 console.log("The sportsCar object: ", sportsCar);
 
-console.log('---- for-in is unreliable ----');
+console.log('---- for-in is unreliable ----\n');
 for (var prop in sportsCar) {
     console.log(prop);
 }
+console.log('Iterating over object AND prototype properties\n');
+
+console.log('---- for-of is reliable ----\n');
+for (var prop of Object.keys(sportsCar)) {
+    console.log(prop + ": " + sportsCar[prop]);
+}
+
+console.log("Iterating over object's OWN PROPERTIES only!\n");
