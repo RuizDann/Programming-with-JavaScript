53e465d5a140d5ccfe834cfb5111ee24252306bd
Daniel Ruiz <ruizdaniel.e@yahoo.com>
2022-09-19 14:03:05 -0500

    new file with no comments

---
+++ For-Of-Loops&Objects/ForOfLoops&ObjectsTwo.js
@@ -0,0 +1,16 @@
+const car = {
+    engine: true
+}
+
+const sportsCar = Object.create(car);
+sportsCar.speed = "fast";
+console.log("The sportsCar object: ", sportsCar);
+
+
+for (var prop in sportsCar) {
+    console.log(prop);
+}
+
+for (var prop of Object.keys(sportsCar)) {
+    console.log(prop + ": " + sportsCar[prop]);
+}
