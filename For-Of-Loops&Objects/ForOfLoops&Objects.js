f7bc673aaf0355f19bdca555b8a2259ad04b371e
Daniel Ruiz <ruizdaniel.e@yahoo.com>
2022-09-19 13:55:37 -0500

    for in loop

--- For-Of-Loops&Objects/ForOfLoops&Objects.js
+++ For-Of-Loops&Objects/ForOfLoops&Objects.js
@@ -7,3 +7,8 @@ const car = {
 const sportsCar = Object.create(car);
 sportsCar.speed = "fast";
 console.log("The sportsCar object: ", sportsCar);
+
+console.log()'---- for-in is unreliable ----');
+for (var prop in sportsCar) {
+    console.log(prop);
+}
