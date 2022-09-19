020cf77a7e157b7a0b4c92a2e311c52449c5bbb5
Daniel Ruiz <ruizdaniel.e@yahoo.com>
2022-09-18 20:33:46 -0500

    finished code

--- Building-OOP-Assignment.js
+++ Building-OOP-Assignment.js
@@ -26,34 +26,45 @@ class Person {
     }
 }
 
-
 // Task 2: Code a Worker class, a sub-class, inheriting from the Person class
 // The Worker class has two additional parameters in the constructor:  xp (for "experience points") & hourlyWage
 // These properties are set to the following default values: xp = 0; hourlyWage = 10;
 // The Worker class has all the paramerters and methods of its super-class.
 // Additionally, it has the goToWork() method, which, whenever it's run, increases the value of the xp property by 10.
-
-
+class Worker extends Person {
+    constructor(xp = 0, hourlyWage = 10, name, age, energy) {
+        super(name, age, energy);
+        this.xp = xp;
+        this.hourlyWage = hourlyWage;
+    }
+    goToWork() {
+        this.xp += 10;
+        // console.log('Experience points are increasing, currently at: ', this.xp, '\n');
+    }
+}
 
 // Task 3: Code a intern object
 // Inside the intern function instantiate the Worker class to code a new intern object.
 // The intern should have the following characteristics: name: Bob; age: 21; energy: 110; xp: 0; hourlyWage: 10;
 // Run the goToWork() method on the intern object. Then return the intern object.
 
-
+function intern() {
+    var intern = new Worker(0, 10, 'Bob', 21, 110);
+    intern.goToWork();
+    return intern;
+}
 
 // Task 4: Code a manager object
 // Inside the manager function instantiate the Worker class to code a new manager object.
 // The manager object should have the following characteristics: name: Alice; age: 30; energy: 120; xp: 100; hourlyWage: 30
 // Run the doSomethingFun() method on the manager object. Then return the manager object.
 
-
-// Task 3a: Code an intern object, run methods
-function intern() {
-
-}
-
-// Task 4a: Code a manager object, methods
 function manager() {
-    
+    var manager = new Worker(100, 30, 'Alice', 30, 120);
+    manager.doSomethingFun();
+    return manager;
 }
+
+// Test the code:
+console.log(intern());
+console.log(manager());
