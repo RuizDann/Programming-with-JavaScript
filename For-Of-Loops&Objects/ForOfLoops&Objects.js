608e04952c1706960e0f99bb18f8b20c4161851d
Daniel Ruiz <ruizdaniel.e@yahoo.com>
2022-09-19 13:55:56 -0500

    fpr in loop

--- For-Of-Loops&Objects/ForOfLoops&Objects.js
+++ For-Of-Loops&Objects/ForOfLoops&Objects.js
@@ -8,7 +8,7 @@ const sportsCar = Object.create(car);
 sportsCar.speed = "fast";
 console.log("The sportsCar object: ", sportsCar);
 
-console.log()'---- for-in is unreliable ----');
+console.log('---- for-in is unreliable ----');
 for (var prop in sportsCar) {
     console.log(prop);
 }
