4386e19d0b1d329c9a04097fe2256ed570a40fc8
Daniel Ruiz <ruizdaniel.e@yahoo.com>
2022-09-23 17:06:14 -0500

    updated

---
+++ Week3/testing/addFive.js
@@ -0,0 +1,7 @@
+// This is a simple function which will make it easier to analyze when I write tests for it
+function addFive(val) {
+    return val + 5;
+}
+
+// added a line to export this function so that it can be used by other files in this project
+module.exports = addFive;
